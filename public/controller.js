bde2a5c2223ec8634ab59c44da7448f07481e69f
Shad Musthafa <shad.musthafa@stellium.com>
2020-09-16 16:09:11 +0530

    hotfix token not found

--- public/controller.js
+++ public/controller.js
@@ -5,23 +5,23 @@ window.onload = function () {
 
     socket.emit("joinRoom", { roomTitle: token });
     
+    function getToken() {
+        let token = window.location.search.match(/token=.*/);
+        if (token) return token[0].split("=").pop();
+        else return "";
+    }
+    function onGrossMarginPress() {
+        let data = {
+            token: token,
+            actions: [
+                {
+                    action: "filter",
+                    value: "GrossMargin",
+                    ts: new Date().getTime()
+                }
+            ]
+        };
+        socket.emit("messageToServer",data);
+    }
 }
 
-function getToken() {
-    let token = window.location.search.match(/token=.*/);
-    if (token) return token[0].split("=").pop();
-    else return "";
-}
-function onGrossMarginPress() {
-    let data = {
-        token: token,
-        actions: [
-            {
-                action: "filter",
-                value: "GrossMargin",
-                ts: new Date().getTime()
-            }
-        ]
-    };
-    socket.emit("messageToServer",data);
-}
